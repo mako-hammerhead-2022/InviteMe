b6c463fb40786265aa42b3b0d40ba82ccad75823
ayoung-lee <103156367+ayoung-lee@users.noreply.github.com>
2022-07-13 18:39:58 +1200

    useState for reordered items

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,32 +1,24 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
-import { fetchFruits } from '../actions'
 import GuestForm from './GuestForm'
 
 import SeatingPlan from './SeatingPlan'
 
 function App() {
-  const fruits = useSelector((state) => state.fruits)
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(fetchFruits())
-  }, [])
+  // const fruits = useSelector((state) => state.fruits)
+  // const dispatch = useDispatch()
+  // useEffect(() => {
+  //   dispatch(fetchFruits())
+  // }, [])
 
   return (
     <>
       <div className="app">
-
         <h1>You are invited to the wedding!</h1>
         <SeatingPlan />
-        <ul>
-          {fruits.map((fruit) => (
-            <li key={fruit}>{fruit}</li>
-          ))}
-        </ul>
 
         <GuestForm />
-
       </div>
     </>
   )
