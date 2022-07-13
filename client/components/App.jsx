5b87c40efa263bfa1a5104038753a39d9ec9a3f4
Angela Yang <kyan211@mywhitecliffe.com>
2022-07-13 18:20:58 +1200

    add navbar

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
-
 import GuestForm from './GuestForm'
+import Navbar from './Navbar'
 import SeatingPlan from './SeatingPlan'
+// import { Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <>
       <div className="app">
-        <h1>You are invited to the wedding!</h1>
-        <SeatingPlan />
+        <Navbar />
         <GuestForm />
+        <SeatingPlan />
+
+        {/* <Router>
+          <Routes>
+            <Route path="/" element={<GuestForm />} />
+            <Route path="/" element={<SeatingPlan />} />
+          </Routes>
+        </Router> */}
       </div>
     </>
   )
---
+++ client/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="top">
+      <div className="topRight">
+        <ul className="topList">
+          <li className="topListItem">RSVP</li>
+          <li className="topListItem">SEATING PLAN</li>
+          <li className="topListItem">GUEST LIST</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
