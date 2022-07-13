bd9ce6046f10039dcd2266c0892ec3d8b89fdd9c
Angela Yang <kyan211@mywhitecliffe.com>
2022-07-13 18:49:57 +1200

    add guestList page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
 import GuestForm from './GuestForm'
 import Navbar from './Navbar'
+import GuestList from './pages/GuestList'
 import SeatingPlan from './SeatingPlan'
-// import { Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <>
       <div className="app">
-        <Navbar />
-        <GuestForm />
-        <SeatingPlan />
-
-        {/* <Router>
+        <Router>
+          <Navbar />
           <Routes>
+            <Route path="/guestlist" element={<GuestList />} />
             <Route path="/" element={<GuestForm />} />
-            <Route path="/" element={<SeatingPlan />} />
+            <Route path="/seatingplan" element={<SeatingPlan />} />
           </Routes>
-        </Router> */}
+        </Router>
       </div>
     </>
   )
