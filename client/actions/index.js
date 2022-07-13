1a6d853d8421ead83660a0b708c06d5121c5282d
beyond_wen_mako_22 <103220308+Beyond-Wen@users.noreply.github.com>
2022-07-13 18:24:08 +1200

    actions up

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,3 +1,18 @@
+export const RECEIVE_GUESTS = 'RECEIVE_GUESTS'
+export const SET_ERROR = 'SET_ERROR'
+export const SET_LOADING = 'SET_LOADING'
+
+import { getAllGuests, deleteGuestApi, addNewGuest } from '../apis'
+
+export const fetchGuests = () => {
+  return (dispatch) => {
+    dispatch(setLoading())
+    return getAllGuests()
+      .then((guests) => dispatch(receiveGuests(guests)))
+      .catch((err) => dispatch(setError(err.message)))
+  }
+}
+
 export const receiveGuests = (guests) => {
   return {
     type: RECEIVE_GUESTS,
@@ -12,9 +27,23 @@ export const setError = (errMessage) => {
   }
 }
 
+export const setLoading = () => {
+  return {
+    type: SET_LOADING,
+  }
+}
+
 export const addGuest = (newGuest) => {
   return (dispatch) => {
     return addNewGuest(newGuest)
+      .then((guests) => dispatch(receiveGuests(guests)))
+      .catch((err) => dispatch(setError(err.message)))
+  }
+}
+
+export const deleteGuest = (id) => {
+  return (dispatch) => {
+    return deleteGuestApi(id)
       .then((guests) => dispatch(receiveGuests(guests)))
       .catch((err) => dispatch(setError(err.message)))
   }
