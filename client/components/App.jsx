1b1cdc487154e4be501a62ba25489f161802772e
Angela Yang <94413896+angelayknz@users.noreply.github.com>
2022-07-13 14:49:18 +1200

    initial commit

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       <div className="app">
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
+        <h1>You are invited to the wedding!</h1>
         <ul>
           {fruits.map((fruit) => (
             <li key={fruit}>{fruit}</li>
