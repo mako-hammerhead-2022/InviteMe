6c095fc933f156d21ba0eea98bae6793bfb63f8c
dhdaveidhd <46812457+dhdaveidhd@users.noreply.github.com>
2022-07-14 13:40:50 +1200

    complete email test

---
+++ server/emailTest.js
@@ -0,0 +1,24 @@
+const nodemailer = require('nodemailer')
+
+const transporter = nodemailer.createTransport({
+  service: 'hotmail',
+  auth: {
+    user: 'invitemetesting@outlook.com',
+    pass: 'DevAcademy1',
+  },
+})
+
+const options = {
+  from: 'invitemetesting@outlook.com',
+  to: 'invitemetesting@outlook.com',
+  subject: 'another test email',
+  text: 'this is a dev test email',
+}
+
+transporter.sendMail(options, function (err, info) {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log('sent: ' + info.response)
+})
