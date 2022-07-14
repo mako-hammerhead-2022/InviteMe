6811dda14974b2c89c86b40f9f49cc243090caea
ayoung-lee <103156367+ayoung-lee@users.noreply.github.com>
2022-07-15 11:53:34 +1200

    file ready to merge with Guests

--- client/reducers/guests.js
+++ client/reducers/guests.js
@@ -5,7 +5,7 @@ const initialState = []
 const guestsReducer = (state = initialState, action) => {
   switch (action) {
     case RECEIVE_GUESTS:
-      return action.movies
+      return action.guests
 
     default:
       return state
