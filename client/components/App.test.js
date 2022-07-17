ec50be8ccd9de3ca1ac4835d8cef337bb702ce03
Angela Yang <kyan211@mywhitecliffe.com>
2022-07-18 11:15:06 +1200

    testing guests reducer

--- client/components/App.test.js
+++ client/components/App.test.js
@@ -1,23 +1,25 @@
-// app.test.js
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import React from 'react'
 import '@testing-library/jest-dom'
-// import {App, LocationDisplay} from './app'
-import App from './App.jsx'
-import Navbar from './Navbar.jsx'
-import { MemoryRouter as Router } from 'react-router-dom'
-import GuestList from './GuestList.jsx'
-import { Provider } from 'react-redux'
+import guests from '../reducers'
+import { setGuest, updateGuest } from '../actions/index.js'
+import { updateRsvpGuest } from '../apis'
 
-describe('<GuestList />', () => {
-  it('shows the guest name', () => {
-    render(
-      <Router>
-        <GuestList />
-      </Router>
-    )
-    let heading = screen.getByRole('heading')
-    expect(heading).toContainHTML('Guest Name')
+describe('guests reducer', () => {
+  it('sets guests data', () => {
+    const oldState = ['david']
+    const action = setGuest(['beyond'])
+    const newState = guests(oldState, action)
+    expect(newState.guests).toEqual(action.payload)
   })
+
+  // it('updates guests data', () => {
+  //   const oldState = [
+  //     { id: 1, name: 'david' },
+  //     { id: 2, name: 'beyond' },
+  //   ]
+  //   const action = updateGuest([{ id: 2, name: 'ayoung' }])
+  //   const newState = guests(oldState, action)
+  //   expect(newState[0].name).toBe('david')
+  //   expect(newState[1].name).toBe('ayoung')
+  //   expect(newState.name).toHaveLength(2)
+  // })
 })
