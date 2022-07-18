4170440167caa178b6c5a606143e1c5075960bf2
rohan-fowlerharper <rohan.fowlerharper@gmail.com>
2022-07-18 13:30:56 +1200

    add redirect on auth callback

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import RSVPForm from './RSVPForm'
-import Navbar from './Navbar'
 import GuestList from './GuestList'
 import SeatingPlan from './SeatingPlan'
 import LoginPage from './LoginPage'
 import NoMatch from './NoMatch'
 import MainLayout from './MainLayout.jsx'
+import Callback from './Callback'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -21,6 +21,7 @@ function App() {
             </Route>
             <Route exact path="/rsvp/:id" element={<RSVPForm />} />
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/callback" element={<Callback />} />
             <Route path="*" element={<NoMatch />} />
           </Routes>
         </Router>
--- client/components/LoginButton.jsx
+++ client/components/LoginButton.jsx
@@ -7,7 +7,7 @@ const LoginButton = () => {
   function handleSignIn(e) {
     e.preventDefault()
     loginWithRedirect({
-      redirectUri: `${window.location.origin}/rsvp/1`,
+      redirectUri: `${window.location.origin}/callback`,
     })
   }
 
