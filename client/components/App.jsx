e63235bd22015265f101249fc3f62eb973b896e3
Angela Yang <kyan211@mywhitecliffe.com>
2022-07-19 08:10:26 +1200

    App tests

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,25 +6,23 @@ import LoginPage from './LoginPage'
 import NoMatch from './NoMatch'
 import MainLayout from './MainLayout.jsx'
 import Callback from './Callback'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <>
       <div className="app">
-        <Router>
-          {/* <Navbar /> */}
-          <Routes>
-            <Route element={<MainLayout />}>
-              <Route exact path="/" element={<GuestList />} />
-              <Route exact path="/seatingplan" element={<SeatingPlan />} />
-            </Route>
-            <Route exact path="/rsvp/:id" element={<RSVPForm />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/callback" element={<Callback />} />
-            <Route path="*" element={<NoMatch />} />
-          </Routes>
-        </Router>
+        {/* <Navbar /> */}
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route exact path="/" element={<GuestList />} />
+            <Route exact path="/seatingplan" element={<SeatingPlan />} />
+          </Route>
+          <Route exact path="/rsvp/:id" element={<RSVPForm />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/callback" element={<Callback />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
       </div>
     </>
   )
--- client/components/NoMatch.jsx
+++ client/components/NoMatch.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function NoMatch() {
-  return <h4>Page not found</h4>
+  return <h4>404 Page not found</h4>
 }
 
 export default NoMatch
