68f502145fbc6643647b6a4c851d45acf3f4f038
dhdaveidhd <46812457+dhdaveidhd@users.noreply.github.com>
2022-07-18 15:59:20 +1200

    chakra setup complete

--- client/index.js
+++ client/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import store from './store'
 import App from './components/App'
 import { Auth0Provider } from '@auth0/auth0-react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 // const domain = process.env.REACT_APP_AUTH0_DOMAIN
 // const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
@@ -17,7 +18,9 @@ document.addEventListener('DOMContentLoaded', () => {
         redirectUri={window.location.origin}
         audience="https://guests/api"
       >
-        <App />
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
       </Auth0Provider>
     </Provider>,
     document.getElementById('app')
