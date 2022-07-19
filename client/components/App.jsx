e20f72f98531447194b11087d347f1bb58c8f89a
Angela Yang <94413896+angelayknz@users.noreply.github.com>
2022-07-19 12:37:15 +1200

    Update App.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,16 +8,15 @@ import MainLayout from './MainLayout.jsx'
 import Callback from './Callback'
 <<<<<<< HEAD
 import { Route, Routes } from 'react-router-dom'
-=======
+
 import LogoutPage from './LogoutPage'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
->>>>>>> dc2666d21b1d6be6379c64bc068a134dd8c67b3e
+
 
 function App() {
   return (
     <>
       <div className="app">
-<<<<<<< HEAD
+
         {/* <Navbar /> */}
         <Routes>
           <Route element={<MainLayout />}>
@@ -28,23 +27,10 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/callback" element={<Callback />} />
           <Route path="*" element={<NoMatch />} />
-        </Routes>
-=======
-        <Router>
-          {/* <Navbar /> */}
-          <Routes>
-            <Route element={<MainLayout />}>
-              <Route exact path="/" element={<GuestList />} />
-              <Route exact path="/seatingplan" element={<SeatingPlan />} />
-            </Route>
-            <Route exact path="/rsvp/:id" element={<RSVPForm />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/callback" element={<Callback />} />
-            <Route path="*" element={<NoMatch />} />
             <Route path="/logout" element={<LogoutPage />} />
-          </Routes>
-        </Router>
->>>>>>> dc2666d21b1d6be6379c64bc068a134dd8c67b3e
+        </Routes>
+
+
       </div>
     </>
   )
